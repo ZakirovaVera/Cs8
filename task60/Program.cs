385649470939e38b2839197a4b129e2f51b99ba7
ZakirovaVera <zakirovavk@gmail.com>
2022-09-01 23:43:03 +0500

    Решение задачи 60

--- task60/Program.cs
+++ task60/Program.cs
@@ -7,12 +7,9 @@ Console.Clear();
 int rows = GetNumberFromUser("Введите число строк массива: ", "Ошибка ввода!");
 int columns = GetNumberFromUser("Введите число столбцов массива: ", "Ошибка ввода!");
 int width = GetNumberFromUser("Введите ширину массива: ", "Ошибка ввода!");
-int startArray = GetNumberFromUser("Введите минимальное число массива: ", "Ошибка ввода!");
-int endArray = GetNumberFromUser("Введите максимальное число массива: ", "Ошибка ввода!");
 
-int[,,] array = GetArray(rows, columns, width, startArray, endArray);
+int[,,] array = GetArray(rows, columns, width);
 PrintArray(array);
-Console.WriteLine();
 
 
 int GetNumberFromUser(string message, string errorMessage)
@@ -28,34 +25,60 @@ int GetNumberFromUser(string message, string errorMessage)
 }
 
 //заполнение массива
-int[,,] GetArray(int row, int column, int width, int minValue, int maxValue)
+int[,,] GetArray(int countRows, int countColumns, int countWidth)
 {
-    int[,,] result = new int[row, column,width];
-    for (int i = 0; i < row; i++)
+    int[] arrRandomNum = GenerateArray(countRows, countColumns, countWidth);
+    Console.WriteLine(string.Join(",", arrRandomNum).TrimEnd(','));
+
+    int[,,] result = new int[countRows, countColumns, countWidth];
+    int nextIndexRandomNumber = 0;
+    for (int k = 0; k < countWidth; k++)
     {
-        for (int j = 0; j < column; j++)
+        for (int i = 0; i < countRows; i++)
         {
-            for (int k = 0; k < width; k++)
+            for (int j = 0; j < countColumns; j++)
             {
-            result[i, j,k] = new Random().Next(minValue, maxValue + 1);
+                result[i, j, k] = arrRandomNum[nextIndexRandomNumber];
+                nextIndexRandomNumber++;
             }
         }
     }
+
     return result;
 }
 
 //вывод массива в консоль
 void PrintArray(int[,,] inArray)
 {
-    for (int i = 0; i < inArray.GetLength(0); i++)
+    for (int k = 0; k < inArray.GetLength(2); k++)
     {
-        for (int j = 0; j < inArray.GetLength(1); j++)
+        for (int i = 0; i < inArray.GetLength(0); i++)
         {
-            for (int k = 0; k < inArray.GetLength(2); k++)
+            for (int j = 0; j < inArray.GetLength(1); j++)
             {
-            Console.Write($"{inArray[i, j, k]}\t");
+                Console.Write($"{inArray[i, j, k]}({i},{j},{k}) ");
             }
+            Console.WriteLine();
         }
         Console.WriteLine();
     }
+}
+
+int[] GenerateArray(int countRows, int countColumns, int countWidth)
+{
+    int lengthThreeArray = countRows * countColumns * countWidth;
+    int[] arrRandomNum = new int[lengthThreeArray];
+    for (int i = 0; i < arrRandomNum.Length; i++)
+    {
+        bool contains = true;
+        int next = 0;
+        while (contains)
+        {
+            next = new Random().Next(10, 99 + 1);
+            contains = arrRandomNum.Contains(next);
+        }
+        arrRandomNum[i] = next;
+    }
+
+    return arrRandomNum;
 }
